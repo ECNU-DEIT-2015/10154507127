6ca9e9dc788976c58be870b95c0e61b39ada3516
sherrytowne <10154507127@ecnu.cn>
2017-10-13 20:13:30 +0800

    修改作业名称

---
+++ eduapp-master/web/index.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+import 'dart:math' as math;
+void main() {
+  querySelector('#sample_text_id')
+    ..text = 'hello'
+    ..onClick.listen(reverseText);
+}
+
+void reverseText(MouseEvent event) {
+  var text = querySelector('#sample_text_id').text;
+  var buffer = new StringBuffer();
+  for (int i = text.length - 1; i >= 0; i--) {
+    buffer.write(text[i]);
+  }
+  querySelector('#sample_text_id').text = buffer.toString();
+  //我做了点修改
+  querySelector("#sample_text_id").text="new text";
+}
